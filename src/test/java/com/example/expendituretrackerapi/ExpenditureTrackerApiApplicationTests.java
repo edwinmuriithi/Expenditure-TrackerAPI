c155c5b1f6d6d88a3ed30731f69c2363a02d230d
edwinmuriithi <kabuimuriithi@gmail.com>
2023-02-17 09:38:50 +0300

    Initial Commit

---
+++ src/test/java/com/example/expendituretrackerapi/ExpenditureTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expendituretrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenditureTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
