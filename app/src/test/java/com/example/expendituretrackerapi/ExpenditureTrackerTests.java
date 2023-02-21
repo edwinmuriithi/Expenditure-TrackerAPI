45954f71576c6a7a54f3f15a8efd959bc047b834
edwinmuriithi <kabuimuriithi@gmail.com>
2023-02-21 12:19:41 +0300

    feat(Module): Create module

--- app/src/test/java/com/example/expendituretrackerapi/ExpenditureTrackerTests.java
+++ app/src/test/java/com/example/expendituretrackerapi/ExpenditureTrackerTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ExpenditureTrackerApiApplicationTests {
+class ExpenditureTrackerTests {
 
 	@Test
 	void contextLoads() {
