c8283ae3b80e38c90fe4ce04344c1e84116e3434
melodytowett <melodytowett99@gmail.com>
2023-02-23 17:03:57 +0300

    added method to post the budget

--- app/src/main/java/com/example/expendituretrackerapi/services/impl/IncomeServiceImpl.java
+++ app/src/main/java/com/example/expendituretrackerapi/services/impl/IncomeServiceImpl.java
@@ -1,7 +1,6 @@
 package com.example.expendituretrackerapi.services.impl;
 
 import com.example.expendituretrackerapi.entities.Income;
-import com.example.expendituretrackerapi.entities.dto.IncomeDTO;
 import com.example.expendituretrackerapi.repositories.IncomeRepository;
 import com.example.expendituretrackerapi.services.IncomeService;
 import lombok.extern.slf4j.Slf4j;
@@ -25,6 +24,7 @@ public class IncomeServiceImpl implements IncomeService {
         income.setIncome(income.getIncome());
         income.setBudget(income.getBudget());
         income.setExpenditure(income.getExpenditure());
+        income.setCreatedDate(income.getCreatedDate());
         Income newIncome = incomeRepository.save(income);
         return newIncome;
     }
