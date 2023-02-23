41faa6dda2db6b65dac25aec85b3b154118b6343
edwinmuriithi <kabuimuriithi@gmail.com>
2023-02-23 12:50:03 +0300

    feat(Expenditure): Create Post method in Expenditure

--- app/src/main/java/com/example/expendituretrackerapi/services/impl/ExpenditureServiceImpl.java
+++ app/src/main/java/com/example/expendituretrackerapi/services/impl/ExpenditureServiceImpl.java
@@ -1,6 +1,30 @@
 package com.example.expendituretrackerapi.services.impl;
 
+import com.example.expendituretrackerapi.entities.Expenditure;
+import com.example.expendituretrackerapi.repositories.ExpenditureRepository;
 import com.example.expendituretrackerapi.services.ExpenditureService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class ExpenditureServiceImpl implements ExpenditureService {
+
+    @Autowired
+    private ExpenditureRepository expenditureRepository;
+
+    public ExpenditureServiceImpl(ExpenditureRepository expenditureRepository) {
+        this.expenditureRepository = expenditureRepository;
+    }
+
+    @Override
+    public Expenditure createExpenditure(Expenditure expenditure) {
+        expenditure.setRent(expenditure.getRent());
+        expenditure.setFood(expenditure.getFood());
+        expenditure.setTransport(expenditure.getTransport());
+        expenditure.setHealth(expenditure.getHealth());
+        expenditure.setSchoolFee(expenditure.getSchoolFee());
+        expenditure.setShopping(expenditure.getShopping());
+        expenditure.setEntertainment(expenditure.getEntertainment());
+        expenditure.setTotal(expenditure.getTotal());
+        Expenditure newExpenditure = expenditureRepository.save(expenditure);
+        return newExpenditure;
+    }
 }
