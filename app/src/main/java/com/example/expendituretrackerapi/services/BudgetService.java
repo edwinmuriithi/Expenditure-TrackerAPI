0e8a61776487bdc6e1d9db6312caccca5deeace0
edwinmuriithi <kabuimuriithi@gmail.com>
2023-02-24 10:58:59 +0300

    feat(Budget): IMplement GET function in budget

--- app/src/main/java/com/example/expendituretrackerapi/services/BudgetService.java
+++ app/src/main/java/com/example/expendituretrackerapi/services/BudgetService.java
@@ -1,4 +1,10 @@
 package com.example.expendituretrackerapi.services;
 
+import com.example.expendituretrackerapi.entities.Budget;
+
+import java.util.List;
+
 public interface BudgetService {
+
+    List<Budget> viewBudget();
 }
