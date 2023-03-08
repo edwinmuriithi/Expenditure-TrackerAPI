1c1c4cbf927e69c0683e56c3ba50eaead9113d70
edwinmuriithi <kabuimuriithi@gmail.com>
2023-03-08 16:46:28 +0300

    update tests

--- app/src/test/java/com/example/expendituretrackerapi/controllers/ExpenditureControllerTest.java
+++ app/src/test/java/com/example/expendituretrackerapi/controllers/ExpenditureControllerTest.java
@@ -1,23 +1,9 @@
 package com.example.expendituretrackerapi.controllers;
 
-import com.example.expendituretrackerapi.entities.Expenditure;
-import com.example.expendituretrackerapi.entities.dto.ExpenditureDTO;
 import com.example.expendituretrackerapi.services.ExpenditureService;
-import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static com.example.expendituretrackerapi.controllers.IncomeControllerTest.asJsonString;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ExpenditureController.class)
 class ExpenditureControllerTest {
