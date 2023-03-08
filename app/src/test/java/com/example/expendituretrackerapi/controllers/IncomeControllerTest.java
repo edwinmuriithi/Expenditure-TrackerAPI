275ac646bcd92b25f2a966a3856d87081fbd5a43
edwinmuriithi <kabuimuriithi@gmail.com>
2023-03-08 14:58:30 +0300

    feat(Controller): Implement integration testing

--- app/src/test/java/com/example/expendituretrackerapi/controllers/IncomeControllerTest.java
+++ app/src/test/java/com/example/expendituretrackerapi/controllers/IncomeControllerTest.java
@@ -2,12 +2,16 @@ package com.example.expendituretrackerapi.controllers;
 
 import com.example.expendituretrackerapi.entities.Income;
 import com.example.expendituretrackerapi.exception.IncomeNotFoundException;
+import com.example.expendituretrackerapi.repositories.IncomeRepository;
 import com.example.expendituretrackerapi.services.IncomeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -19,7 +23,7 @@ import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -28,8 +32,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class IncomeControllerTest extends AbstractTest{
     @Autowired
     private MockMvc mockMvc;
-    @Autowired
+    @MockBean
     private IncomeService incomeService;
+    @MockBean
+    private IncomeRepository incomeRepositoryS;
 
     @BeforeEach
     public void setup() throws Exception {
@@ -43,9 +49,16 @@ class IncomeControllerTest extends AbstractTest{
         when(incomeService.findById(2L)).thenThrow(new IncomeNotFoundException("Campaign not found with ID " + 2L));
 
     }
+
+        @Test
+        @DisplayName("Create Income tests")
+        void createIncomeTest() throws Exception{
+
+        }
+
         @Test
         @DisplayName("View Income by ID success")
-        void viewIncomeByIdSuccessTest() throws Exception{
+        void viewIncomeByIdTest() throws Exception{
             MockHttpServletRequestBuilder requestBuilder = get("/income/1")
                     .contentType(MediaType.APPLICATION_JSON);
             ResultActions resultActions = mockMvc.perform(requestBuilder).andDo(print()).andExpect(status().is2xxSuccessful());
@@ -54,6 +67,17 @@ class IncomeControllerTest extends AbstractTest{
 
             MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/income/1")
                     .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        }
+        @Test
+        @DisplayName("Delete Income By ID Success")
+        void deleteIncomeByIdSuccess() throws Exception {
+    //         when
+            MockHttpServletRequestBuilder requestBuilder =
+                    delete("/income/2")
+                            .contentType(MediaType.APPLICATION_JSON);
+            ResultActions result =
+                    mockMvc.perform(requestBuilder).andDo(print()).andExpect(status().is2xxSuccessful());
 
         }
 
