3ef3e4574eb4e0ab2473f6da772d89abca630716
edwinmuriithi <kabuimuriithi@gmail.com>
2023-03-20 11:32:37 +0300

    Debug Tests

--- app/src/main/java/com/example/expendituretrackerapi/ExpenditureTracker.java
+++ app/src/main/java/com/example/expendituretrackerapi/ExpenditureTracker.java
@@ -2,10 +2,14 @@ package com.example.expendituretrackerapi;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableAutoConfiguration
 @SpringBootApplication
+@EnableJpaRepositories("com.example.expendituretrackerapi.repositories")
 public class ExpenditureTracker {
 	@Bean
 	public ModelMapper modelMapper(){
