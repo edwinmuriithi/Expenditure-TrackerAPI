717353a61cdf39a4889ac393add04cf03ad154f0
melodytowett <melodytowett99@gmail.com>
2023-03-21 14:32:14 +0300

    debuging tests

--- app/src/test/java/com/example/expendituretrackerapi/repositories/IncomeRepositoryTest.java
+++ app/src/test/java/com/example/expendituretrackerapi/repositories/IncomeRepositoryTest.java
@@ -1,27 +1,47 @@
-//package com.example.expendituretrackerapi.repositories;
-//
-//import com.example.expendituretrackerapi.entities.Income;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import java.time.LocalDate;
-//import java.util.Optional;
-//
-//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//@SpringBootTest
-//class IncomeRepositoryTest {
-//
-//        @Autowired
-//        private IncomeRepository incomeRepository;
-//
-//        @Test
-//        @DisplayName("Tests findById Income")
-//        public void findByIdIncome() {
-//            // Given
+package com.example.expendituretrackerapi.repositories;
+
+import com.example.expendituretrackerapi.entities.Income;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class IncomeRepositoryTest {
+
+        @Autowired
+        private IncomeRepository incomeRepository;
+        private Income income;
+        @BeforeEach
+        public void setup(){
+            income = Income.builder()
+                    .id(1L)
+                    .budget(100000)
+                    .income(400000)
+                    .build();
+        }
+
+        @Test
+        @DisplayName("Save Income")
+        public void saveIncome(){
+            Income newIncome = incomeRepository.save(income);
+            assertThat(newIncome).isNotNull();
+            assertThat(newIncome.getIncome()).isGreaterThan(0);
+        }
+        @Test
+        @DisplayName("Tests findById Income")
+        public void findByIdIncome() {
+            // Given
 //            Income income = new Income(1L, 50000,20000, LocalDate.now());
 //            incomeRepository.save(income);
 //
@@ -33,5 +53,5 @@
 //            assertThat(foundIncome.get().getIncome()).isEqualTo(income.getIncome());
 //            assertThat(foundIncome.get().getBudget()).isEqualTo(income.getBudget());
 //            assertThat(foundIncome.get().getCreatedDate()).isEqualTo(income.getCreatedDate());
-//        }
-//    }
+        }
+    }
